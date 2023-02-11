280492804193ac1ef5526e8504fb1fd1b1867e62
respat <respergerpatrik@gmail.com>
2023-02-11 20:22:10 +0100

    Fájlok módosítva, részben formázva

--- src/app.js
+++ src/app.js
@@ -7,3 +7,21 @@
 * Github: https://github.com/respat/
 * Licenc: GNU GPL
 */
+
+hossz = document.querySelector('#hossz');
+elotolas = document.querySelector('#elotolas');
+rpm = document.querySelector('#rpm');
+ido = document.querySelector('#ido');
+kiszamol = document.querySelector('#kiszamol');
+
+
+function kiszamit()
+{
+    megmunkalasiIdo = (hossz.value / (rpm.value * elotolas.value));
+    ido.value = megmunkalasiIdo;
+    console.log(megmunkalasiIdo);
+} 
+
+kiszamol.addEventListener("click", () => {
+    kiszamit();
+ });
