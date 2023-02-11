508257e5d047d939fad9c57f695e8d86793fc1a6
respat <respergerpatrik@gmail.com>
2023-02-11 10:27:30 +0100

    Névjegy hozzáadva

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Resperger Patrik
+* Copyright: 2023, Resperger Patrik
+* Group: Szoft I/1/E
+* Date: 2023-02-11
+* Github: https://github.com/respat/
+* Licenc: GNU GPL
+*/
